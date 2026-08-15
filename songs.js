cf93430f8f6f8715f073b17101afe1def734b5a2
salmanvisual19-rgb <salmanvisual19@gmail.com>
2026-08-15 22:25:32 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,272 @@
+window.SONGS = [
+  {
+    "name": "Aankhon Se Tune 2.0 (From \"Bhai Tera Star Hai\") - Aankhon Se Tune 2.0 (From Bhai Tera Star Hai) (320 kbps) - 26410",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Aankhon%20Se%20Tune%202.0%20(From%20%22Bhai%20Tera%20Star%20Hai%22)%20-%20Aankhon%20Se%20Tune%202.0%20(From%20Bhai%20Tera%20Star%20Hai)%20(320%20kbps)%20-%2026410.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Aawaara Angaara - Tere Ishk Mein (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Aawaara%20Angaara%20-%20Tere%20Ishk%20Mein%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "ABOUT ME - About Me (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/ABOUT%20ME%20-%20About%20Me%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Bairi - Bairi (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Bairi%20-%20Bairi%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Barbaad - Saiyaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Barbaad%20-%20Saiyaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Bekhayali - Kabir Singh (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Bekhayali%20-%20Kabir%20Singh%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "BLACK RIDE - BLACK RIDE (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/BLACK%20RIDE%20-%20BLACK%20RIDE%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Blender  (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Blender%20%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Dastaan ",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Dastaan%20.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Deewaana Deewaana - Tere Ishk Mein (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Deewaana%20Deewaana%20-%20Tere%20Ishk%20Mein%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Desi Hood - Desi Hood (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Desi%20Hood%20-%20Desi%20Hood%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Desi Kalakaar - Desi Kalakaar (320 kbps) (1)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Desi%20Kalakaar%20-%20Desi%20Kalakaar%20(320%20kbps)%20(1).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Desi Kalakaar - Desi Kalakaar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Desi%20Kalakaar%20-%20Desi%20Kalakaar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Dhak Dhak Reloaded - Maa Behen (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Dhak%20Dhak%20Reloaded%20-%20Maa%20Behen%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Dhara 302 - Dhara 302 (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Dhara%20302%20-%20Dhara%20302%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Dhun - Saiyaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Dhun%20-%20Saiyaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Do Pal - Veer-Zaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Do%20Pal%20-%20Veer-Zaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Dope Shope - International Villager (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Dope%20Shope%20-%20International%20Villager%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Falani - Falani (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Falani%20-%20Falani%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Financer - Financer (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Financer%20-%20Financer%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Gaadi 150 - Gaadi 150 (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Gaadi%20150%20-%20Gaadi%20150%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Gehra Hua - Dhurandhar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Gehra%20Hua%20-%20Dhurandhar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Humsafar - Saiyaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Humsafar%20-%20Saiyaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Ishq Jalakar - Karvaan - Dhurandhar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Ishq%20Jalakar%20-%20Karvaan%20-%20Dhurandhar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Jailer - Jailer (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Jailer%20-%20Jailer%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Kale Kagaz - Kale Kagaz (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Kale%20Kagaz%20-%20Kale%20Kagaz%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Khat - Khat (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Khat%20-%20Khat%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Khatole 2 - Khatole 2 (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Khatole%202%20-%20Khatole%202%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Love Dose - Desi Kalakaar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Love%20Dose%20-%20Desi%20Kalakaar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Lutt Le Gaya - Dhurandhar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Lutt%20Le%20Gaya%20-%20Dhurandhar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Madhosh (From \"Toxic\") - Madhosh (From Toxic) (320 kbps) - 26405",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Madhosh%20(From%20%22Toxic%22)%20-%20Madhosh%20(From%20Toxic)%20(320%20kbps)%20-%2026405.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Main Yahaan Hoon - Veer-Zaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Main%20Yahaan%20Hoon%20-%20Veer-Zaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Maruti - Maruti (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Maruti%20-%20Maruti%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Move - Yeh Ishq Ishq - Dhurandhar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Move%20-%20Yeh%20Ishq%20Ishq%20-%20Dhurandhar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Naam Chale - Naam Chale (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Naam%20Chale%20-%20Naam%20Chale%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Not Guilty - Not Guilty (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Not%20Guilty%20-%20Not%20Guilty%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Om Shanti Om (Medley Mix)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Om%20Shanti%20Om%20(Medley%20Mix).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Raat Ke Shikari - Raat Ke Shikari (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Raat%20Ke%20Shikari%20-%20Raat%20Ke%20Shikari%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Ramba Ho - Dhurandhar (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Ramba%20Ho%20-%20Dhurandhar%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Saiyaara - Saiyaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Saiyaara%20-%20Saiyaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Sitaare (From \"Ikkis\") - Ikkis (320 kbps) - 359",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Sitaare%20(From%20%22Ikkis%22)%20-%20Ikkis%20(320%20kbps)%20-%20359.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Surma - Surma (320 kbps) - 26797",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Surma%20-%20Surma%20(320%20kbps)%20-%2026797.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Tension - Tension (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Tension%20-%20Tension%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Tera Mera Rishta Awarapan 2",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Tera%20Mera%20Rishta%20Awarapan%202.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Tere Piche - Tere Piche (320 kbps) - 26764",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Tere%20Piche%20-%20Tere%20Piche%20(320%20kbps)%20-%2026764.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Tum Ho Toh - Saiyaara (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Tum%20Ho%20Toh%20-%20Saiyaara%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Tum Tak - Raanjhanaa (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Tum%20Tak%20-%20Raanjhanaa%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Ucha Lamba Kad Forever (From \"Welcome To The Jungle\") - Ucha Lamba Kad Forever (From Welcome To The Jungle) (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Ucha%20Lamba%20Kad%20Forever%20(From%20%22Welcome%20To%20The%20Jungle%22)%20-%20Ucha%20Lamba%20Kad%20Forever%20(From%20Welcome%20To%20The%20Jungle)%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Ve Haaniyaan - Ve Haaniyaan (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Ve%20Haaniyaan%20-%20Ve%20Haaniyaan%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Ve Junoon (From \"Awarapan 2\") - Ve Junoon (From Awarapan 2) (320 kbps) - 26224",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Ve%20Junoon%20(From%20%22Awarapan%202%22)%20-%20Ve%20Junoon%20(From%20Awarapan%202)%20(320%20kbps)%20-%2026224.mp3",
+    "duration": 0
+  },
+  {
+    "name": "Warning - Warning (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Warning%20-%20Warning%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Yaari - Yaari (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Yaari%20-%20Yaari%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Yamraj - Yamraj (320 kbps)",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Yamraj%20-%20Yamraj%20(320%20kbps).mp3",
+    "duration": 0
+  },
+  {
+    "name": "Yeh Awarapan (From \"Awarapan 2\") - Yeh Awarapan (From Awarapan 2) (320 kbps) - 26402",
+    "url": "https://ifcdrbvimiuxsimajwje.supabase.co/storage/v1/object/public/Songs/Yeh%20Awarapan%20(From%20%22Awarapan%202%22)%20-%20Yeh%20Awarapan%20(From%20Awarapan%202)%20(320%20kbps)%20-%2026402.mp3",
+    "duration": 0
+  }
+];
